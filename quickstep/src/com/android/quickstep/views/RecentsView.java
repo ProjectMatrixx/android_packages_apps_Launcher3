f82dfa41294f835ba9cda699c8dc1b3610999801
SagarMakhar <sagarmakhar@gmail.com>
2021-08-27 16:50:06 +0000

    Launcher3: Allow locking tasks to recents [2/2]
    - Drawables are from similar commit of oreo times
    
    Change-Id: Ib444ab50eeeb4fafdf0492edef5bed471fa1753a
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -98,11 +98,13 @@ import android.graphics.PointF;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Typeface;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.os.VibrationEffect;
+import android.provider.Settings;
 import android.text.Layout;
 import android.text.StaticLayout;
 import android.text.TextPaint;
@@ -123,6 +125,7 @@ import android.view.ViewTreeObserver.OnScrollChangedListener;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.view.animation.Interpolator;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.OverScroller;
 import android.widget.Toast;
@@ -219,6 +222,7 @@ import com.android.systemui.shared.system.TaskStackChangeListener;
 import com.android.systemui.shared.system.TaskStackChangeListeners;
 import com.android.wm.shell.common.pip.IPipAnimationListener;
 
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -745,6 +749,15 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
     @Nullable
     private DesktopRecentsTransitionController mDesktopRecentsTransitionController;
 
+    Drawable mLockedDrawable;
+    Drawable mUnlockedDrawable;
+
+    List<String> mLockedTasks = new ArrayList<>();
+
+    private Button mLockButtonView;
+
+    private String mStartPkg, mEndPkg;
+
     /**
      * Keeps track of the desktop task. Optional and only present when the feature flag is enabled.
      */
@@ -838,6 +851,17 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
 
         // Initialize quickstep specific cache params here, as this is constructed only once
         mActivity.getViewCache().setCacheSize(R.layout.digital_wellbeing_toast, 5);
+
+        String lockedTasks = Settings.System.getStringForUser(
+                    context.getContentResolver(),
+                    Settings.System.RECENTS_LOCKED_TASKS,
+                    UserHandle.USER_CURRENT);
+
+        if (mLockedTasks.size() == 0 && lockedTasks != null && !lockedTasks.isEmpty()) {
+            mLockedTasks = new ArrayList<String>(Arrays.asList(lockedTasks.split(",")));
+        }
+        mLockedDrawable = context.getDrawable(R.drawable.recents_locked);
+        mUnlockedDrawable = context.getDrawable(R.drawable.recents_unlocked);
 
         mTintingColor = getForegroundScrimDimColor(context);
 
@@ -1075,6 +1099,8 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
         mSplitSelectStateController = splitController;
         mDesktopRecentsTransitionController = desktopRecentsTransitionController;
         mMemInfoView = memInfoView;
+        mLockButtonView = (Button) mActionsView.findViewById(R.id.action_lock);
+        mLockButtonView.setOnClickListener(this::lockCurrentTask);
     }
 
     public SplitSelectStateController getSplitSelectController() {
@@ -1471,8 +1497,8 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
     @Override
     protected void onPageBeginTransition() {
         super.onPageBeginTransition();
-        if (!mActivity.getDeviceProfile().isTablet) {
-            mActionsView.updateDisabledFlags(OverviewActionsView.DISABLED_SCROLLING, true);
+        if (getCurrentPageTaskView() != null) {
+            mStartPkg = getCurrentPageTaskView().getTask().key.getPackageName();
         }
         if (mOverviewStateEnabled) { // only when in overview
             InteractionJankMonitorWrapper.begin(/* view= */ this, Cuj.CUJ_RECENTS_SCROLLING);
@@ -1482,10 +1508,8 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
     @Override
     protected void onPageEndTransition() {
         super.onPageEndTransition();
-        ActiveGestureLog.INSTANCE.addLog(
-                "onPageEndTransition: current page index updated", getNextPage());
-        if (isClearAllHidden() && !mActivity.getDeviceProfile().isTablet) {
-            mActionsView.updateDisabledFlags(OverviewActionsView.DISABLED_SCROLLING, false);
+        if (getCurrentPageTaskView() != null) {
+            mEndPkg = getCurrentPageTaskView().getTask().key.getPackageName();
         }
         if (getNextPage() > 0) {
             setSwipeDownShouldLaunchApp(true);
@@ -1935,6 +1959,8 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
             if (i == stubRunningTaskIndex) {
                 continue;
             }
+            TaskView tv = getTaskViewAt(i);
+            if (mLockedTasks.contains(tv.getTask().key.getPackageName())) continue;
             removeView(requireTaskViewAt(i));
         }
         if (getTaskViewCount() == 0 && indexOfChild(mClearAllButton) != -1) {
@@ -4086,6 +4112,8 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
 
         int count = getTaskViewCount();
         for (int i = 0; i < count; i++) {
+            TaskView tv = getTaskViewAt(i);
+            if (mLockedTasks.contains(tv.getTask().key.getPackageName())) continue;
             addDismissedTaskAnimations(requireTaskViewAt(i), duration, anim);
         }
 
@@ -4191,6 +4219,22 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
         if (taskView != null) {
             dismissTask(taskView, true /*animateTaskView*/, true /*removeTask*/);
         }
+    }
+
+    private void lockCurrentTask(View view) {
+        TaskView taskView = getCurrentPageTaskView();
+        if (taskView != null) {
+            Task t = taskView.getTask();
+            String pkg = t.key.getPackageName();
+            if (mLockedTasks.contains(pkg)) {
+                mLockedTasks.remove(pkg);
+            } else {
+                mLockedTasks.add(pkg);
+            }
+        }
+        Settings.System.putStringForUser(getContext().getContentResolver(),
+        Settings.System.RECENTS_LOCKED_TASKS, String.join(",", mLockedTasks),
+                UserHandle.USER_CURRENT);
     }
 
     @Override
