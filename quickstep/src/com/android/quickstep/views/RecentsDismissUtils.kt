7734bb5f7e2daa243086159affb489f29cf97fe9
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-04 06:38:20 +0800

    [PERF] snappier recents dismiss
    Change-Id: I040b24bae3f594815984f434f2a4db28f9d8afc8

--- quickstep/src/com/android/quickstep/views/RecentsDismissUtils.kt
+++ quickstep/src/com/android/quickstep/views/RecentsDismissUtils.kt
@@ -61,7 +61,7 @@ class RecentsDismissUtils(private val recentsView: RecentsView<*, *>) {
             )
         val minVelocity =
             recentsView.pagedOrientationHandler.getSecondaryDimension(draggedTaskView).toFloat()
-        val startVelocity = abs(velocity).coerceAtLeast(minVelocity) * velocity.sign
+        val startVelocity = abs(velocity).coerceAtLeast(minVelocity) * velocity.sign * 1.4f
         // Animate dragged task towards dismissal or rest state.
         val draggedTaskViewSpringAnimation =
             SpringAnimation(draggedTaskView, taskDismissFloatProperty)
@@ -293,7 +293,7 @@ class RecentsDismissUtils(private val recentsView: RecentsView<*, *>) {
                     dampingRatioOffset
             )
             .setStiffness(
-                resourceProvider.getFloat(R.dimen.expressive_dismiss_task_trans_y_stiffness)
+                SpringForce.STIFFNESS_HIGH
             )
     }
 
